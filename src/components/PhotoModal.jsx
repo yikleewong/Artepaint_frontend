9590b8aa46c2546007aab73663687f8b822dfb3a
yikleewong <107070504+yikleewong@users.noreply.github.com>
2025-02-25 05:01:16 +0800

    Update PhotoModal.jsx

--- src/components/PhotoModal.jsx
+++ src/components/PhotoModal.jsx
@@ -12,7 +12,7 @@ const PhotoModal = ({ photo, onClose }) => {
       <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center z-50" onClick={handleClickOutside}>
         <div className="w-auto h-auto relative max-h-full max-w-full flex items-center justify-center overflow-hidden">
           <button onClick={onClose} className="absolute top-1 right-1 text-white text-2xl">&times;</button>
-          <img src={`http://localhost:5000${photo}`} alt="Selected Photo" className=" h-full rounded object-contain w-full" />
+          <img src={`https://artepaint-server.herokuapp.com${photo}`} alt="Selected Photo" className=" h-full rounded object-contain w-full" />
         </div>
       </div>
     );
