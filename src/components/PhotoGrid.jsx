cf0f7eae17d5a5221be910f3b2d7db4af4b83518
yikleewong <107070504+yikleewong@users.noreply.github.com>
2025-02-25 05:01:01 +0800

    Update PhotoGrid.jsx

--- src/components/PhotoGrid.jsx
+++ src/components/PhotoGrid.jsx
@@ -6,7 +6,7 @@ const PhotoGrid = ({ photos, onPhotoClick }) => {
       {photos.map((photo, index) => (
         <img
           key={index}
-          src={`http://localhost:5000${photo}`}
+          src={`https://artepaint-server.herokuapp.com${photo}`}
           alt={`Photo ${index + 1}`}
           className="w-full md:h-64 h-40 object-cover cursor-pointer"
           onClick={() => onPhotoClick(photo)}
