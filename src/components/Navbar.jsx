97e2af414b2b7b0b7563c9516e1bb60b5daba190
yikleewong <wongyikleehk@gmail.com>
2025-03-01 02:45:33 +0800

    logo changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.jpg"
 import { useState } from 'react'
 import {
   Bars3Icon,
