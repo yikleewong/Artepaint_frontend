98dabad7aca54118c321dbd627e9a08d48e92009
yikleewong <107070504+yikleewong@users.noreply.github.com>
2025-04-01 17:10:27 +0800

    Update PhotoModal.jsx
    adjust photo size in big screen

--- src/components/PhotoModal.jsx
+++ src/components/PhotoModal.jsx
@@ -10,7 +10,7 @@ const PhotoModal = ({ photo, onClose }) => {
 
     return (
       <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center z-50" onClick={handleClickOutside}>
-        <div className="w-auto h-auto relative max-h-full max-w-full flex items-center justify-center overflow-hidden">
+        <div className="w-auto h-auto lg:h-dvh relative max-h-full max-w-full flex items-center justify-center overflow-hidden">
           <button onClick={onClose} className="absolute top-1 right-1 text-white text-2xl">&times;</button>
           <img src={`https://artepaint-server-876d8e16c9a8.herokuapp.com${photo}`} alt="Selected Photo" className=" h-full rounded object-contain w-full" />
         </div>
